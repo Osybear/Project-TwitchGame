37ade993536207ba1d2ac3eda7d025dd1a66c98f
Osybear <osybeargames@gmail.com>
2019-04-03 16:59:16 -0500

    Update Secrets.cs

--- Assets/Scripts/Secrets.cs
+++ Assets/Scripts/Secrets.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class Secrets {
-    public static string client_id = "0sdugxys7rzgaf1kfqqm7pztdjzatb";
-    public static string client_secret = "xzwuk0sy2tht0ut587okyhujhdrlqj";
-    public static string bot_access_token = "s25tksvufw6nk7t7320usolohmgnhw";
-    public static string bot_refresh_token = "71ix9obqp7xnv8jmo6sn3vknsg5d4m0raxdci1zkcf5j0myobz";
+    public static string client_id = "";
+    public static string client_secret = "";
+    public static string bot_access_token = "";
+    public static string bot_refresh_token = "";
 }
