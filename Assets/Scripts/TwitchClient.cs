45ccd26067591b40da6ca54ee7d2eb6ee7209073
Osybear <osybeargames@gmail.com>
2019-04-03 15:27:56 -0500

    Update
    Twitch Chat Hello World!

---
+++ Assets/Scripts/TwitchClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TwitchLib.Client.Models;
+using TwitchLib.Unity;
+
+public class TwitchClient : MonoBehaviour {
+
+    public Client client;
+    private string channel_name = "osybear";
+
+    private void Start() {
+        Application.runInBackground = true;
+
+        ConnectionCredentials credentials = new ConnectionCredentials("osybearbot", Secrets.bot_access_token);
+        client = new Client();
+        client.Initialize(credentials, channel_name);
+
+        client.Connect();
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            client.SendMessage(client.JoinedChannels[0], "Hello World!");
+            Debug.Log("Send Test Message");
+        }
+    }
+}
